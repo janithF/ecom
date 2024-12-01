c7437bb3ace5c857caf29a4bd35633cb0287a995
Janith Fernando <janithnfernando@gmail.com>
2024-12-01 17:32:15 +0530

    add logo to the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
-import { Box, Flex, HStack } from "@chakra-ui/react";
+import { Box, Flex, HStack, Image } from "@chakra-ui/react";
+import logo from '../assets/logo.svg';
 import SearchInput from "./SearchInput";
 
 interface Props {
@@ -8,9 +9,9 @@ interface Props {
 const Navbar = ({ onSearch }: Props) => {
   return (
     <Flex height={"100%"} alignItems={"center"} justifyContent={"space-between"}>
-      <Box p="3" w="300px" height={"100%"}>
-        Vendoora
-      </Box>
+      <Flex width={"300px"} justifyContent={"center"} alignItems={"center"}>
+        <Image src={logo} width={"130px"}></Image>
+      </Flex>
       <Box w="35%" height={"100%"} p="4px 0">
         <SearchInput onsearch={onSearch} />
       </Box>
